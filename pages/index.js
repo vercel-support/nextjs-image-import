63a77251ba91098decc9ac6aa0162dbeb2498295
Ismael Rumzan <ismael.rumzan@gmail.com>
2021-10-10 17:47:57 +0300

    added import image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import wadirum from '../images/rita-IDYdmpjQXZQ-unsplash.jpg'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Image src={wadirum} alt="Wadi rum"/>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
